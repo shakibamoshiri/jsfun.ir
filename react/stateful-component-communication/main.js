93cf0d9767e4371526f9bd8533ec5b96f5f38ebc
Shakiba Moshiri <hi@shakiba.it>
2019-06-29 17:49:31 +0430

    Add new content for stateful-compnent-communication

---
+++ react/stateful-component-communication/main.js
@@ -0,0 +1,16 @@
+import React, { Fragment, Component } from "react";
+import { render } from "react-dom";
+const log = console.log;
+
+const Prism = ( props ) => <pre><code className={ `language-${props.language || "javascript" }` }>{ props.children }</code></pre>;
+const Title = ( props ) => <h1 className="title">{ props.children }</h1>;
+const SubTitle = ( props ) => <h3 className="sub-title">{ props.children }</h3>;
+
+const root = <Fragment>
+    <p>Not yet.</p>
+    <p>Not yet.</p>
+    <p>Not yet.</p>
+</Fragment>
+
+const div_id_root = document.getElementById( "root" );
+render( root, div_id_root );
