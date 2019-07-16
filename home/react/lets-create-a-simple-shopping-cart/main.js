db2961bb4b338dab07f55aa0a5ae19f2b4063ed6
Shakiba Moshiri <hi@shakiba.it>
2019-07-16 20:06:02 +0430

    Add js for lets-create...

---
+++ home/react/lets-create-a-simple-shopping-cart/main.js
@@ -0,0 +1,15 @@
+import React, { Fragment, Component } from "react";
+import { render } from "react-dom";
+const log = console.log;
+
+const Prism = ( props ) => <pre><code className={ `language-${props.language || "javascript" }` }>{ props.children }</code></pre>;
+const Title = ( props ) => <h1 className="title">{ props.children }</h1>;
+const SubTitle = ( props ) => <h3 className="sub-title">{ props.children }</h3>;
+
+const root = <Fragment>
+    <p>First test with new version</p>
+    <p>First test with new version</p>
+</Fragment>
+
+const div_id_root = document.getElementById( "root" );
+render( root, div_id_root );
